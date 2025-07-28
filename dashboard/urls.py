7396c99586228344fa123e2b7dc06012cf47ef1d
pokheri <bolleanbull@gmail.com>
2025-07-28 13:46:41 +0530

    Subject Progress Page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexPageView.as_view(), name='index'), 
+    path('base/', views.Base.as_view()), 
+    path('progress/', views.ProgressTemplateView.as_view(), name='progress'), 
+    
+
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model 
+from django.views.generic import TemplateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def index(request):
-  return render(request, 'dashboard/student/index.html')
+class IndexPageView(LoginRequiredMixin,TemplateView):
+  template_name = 'dashboard/student/index.html'
+
+class Base(TemplateView):
+  template_name = 'dashboard/student/student_base.html'
+
+class ProgressTemplateView(TemplateView):
+  template_name = 'dashboard/student/progress.html'
+
+
+
+
